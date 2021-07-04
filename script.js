9bb27ff29a7a11603e63074bdba4109f94618699
luisfmiki@hotmail.com <jooj@pop-os.localdomain>
2021-07-04 02:17:13 -0300

    topnav

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 function myFunction() {
     var x = document.getElementById("myTopnav");
+    var barra_verde = document.getElementById("highlight");
     if (x.className === "topnav") {
       x.className += " responsive";
+      barra_verde.style.display = "none";
     } else {
       x.className = "topnav";
+      barra_verde.style.display = "inline-block";
     }
   } 
 
-function escondeMenu(e) {
-    console.log(this)
-    console.log("teste");
+function escondeMenu() {
+    var x = document.getElementById("myTopnav");
+    var barra_verde = document.getElementById("highlight");
+    if (x.className !== "topnav") {
+      x.className = "topnav";
+      barra_verde.style.display = "inline-block";
+    }
 }
 
-document.addEventListener("click", escondeMenu)
