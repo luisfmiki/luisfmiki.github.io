599cd93f917c0bf92a1a058bfd1f429cb5547505
luisfmiki <luisfmiki@hotmail.com>
2024-01-05 02:16:27 -0300

    initial-setup

---
+++ js/language-switch.js
@@ -0,0 +1,73 @@
+// language-switch logic
+// ToDo:
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Function to set the language
+    function setLanguage(lang) {
+        const xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    const data = JSON.parse(xhr.responseText);
+
+                    iterateJson(data);
+
+                    // Update language switch button text
+                    updateLanguageButtonText(lang);
+
+                    // Save the language preference to localStorage
+                    localStorage.setItem('currentLang', lang);
+                } else {
+                    console.error('Error fetching JSON. Status:', xhr.status);
+                }
+            }
+        };
+
+        xhr.open('GET', `../../locales/${lang}.json`, true);
+        xhr.send();
+    }
+
+    // Function to update language switch button text
+    function updateLanguageButtonText(lang) {
+        const langSwitchBtn = document.getElementById('lang-switch-btn');
+
+        // Check if the button element exists
+        if (langSwitchBtn) {
+            // Set button text based on the language state
+            langSwitchBtn.innerText = lang === 'en' ? '日本語 🇯🇵' : 'English 🇺🇸';
+        } else {
+            console.error('Error: Language switch button not found. Check your HTML structure.');
+        }
+    }
+
+    // Event listener for language switch button
+    const langSwitchBtn = document.getElementById('lang-switch-btn');
+    if (langSwitchBtn) {
+        langSwitchBtn.addEventListener('click', function () {
+            // Toggle between English and Japanese
+            const currentLang = localStorage.getItem('currentLang') || 'en';
+            const newLang = currentLang === 'en' ? 'jp' : 'en';
+
+            // Set the new language
+            document.documentElement.lang = newLang;
+
+            // Load content for the new language
+            setLanguage(newLang);
+        });
+    } else {
+        console.error('Error: Language switch button not found. Check your HTML structure.');
+    }
+
+    function iterateJson(data) {
+        const page = window.location.pathname;
+        for (const key in data[page]) {
+            document.getElementById(key).innerText = data[page][key];
+        }
+    }
+
+    // Initial setup - load content for the stored language preference
+    const storedLang = localStorage.getItem('currentLang') || 'en'; // default lang == en
+    document.documentElement.lang = storedLang;
+    setLanguage(storedLang);
+
+});
